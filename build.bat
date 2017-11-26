056f513aba958070ea6a0d9a60b644a0b607bde9
speadup <speadup@gmail.com>
2017-11-26 14:33:50 +0800

    add sendmail.rc

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 del /q /s *.dcu *.dres dcc*.cfg
 cscript /nologo build.js win32
+cgrc -c65001 sendmail.rc -fosendmail.res 
 dcc32 -q -w -$O- -E.  sendmail.dpr
 move sendmail.exe sendmail32.exe
 
 del /q /s *.dcu *.dres dcc*.cfg
 cscript /nologo build.js win64
+cgrc -c65001 sendmail.rc -fosendmail.res 
 dcc64 -q -w -$O- -E.  sendmail.dpr
 move sendmail.exe sendmail64.exe
 pause
