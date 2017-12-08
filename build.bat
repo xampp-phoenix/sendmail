5a05b38fa94dbefb30fe9a7bd8f71dd8b1052406
speadup <speadup@gmail.com>
2017-12-08 23:03:16 +0800

    fix build script

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 cscript /nologo build.js win32
 cgrc -c65001 sendmail.rc -fosendmail.res 
-dcc32 -q -w -$O- -E.  sendmail.dpr
+dcc32 -q -b -w -$O+ -E.  sendmail.dpr
 move sendmail.exe sendmail32.exe
 
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 cscript /nologo build.js win64
 cgrc -c65001 sendmail.rc -fosendmail.res 
-dcc64 -q -w -$O- -E.  sendmail.dpr
+dcc64 -q -b -w -$O+ -E.  sendmail.dpr
 move sendmail.exe sendmail64.exe
 
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 if not defined nopause pause
